cc68d9bb01383b3b4bc564e69d27a72250831de8
danewheaton <wheaton.dane@gmail.com>
2017-08-22 09:19:44 -0500

    deleted old proj

---
+++ monkeyTest/Assets/Scripts/MonkeyJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeyJump : MonoBehaviour
+{
+    [SerializeField] float jumpForce = 500;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+    }
+
+    void Jump()
+    {
+        if (MonkeyStates.currentState != MonkeyIs.JUMPING)
+        {
+            //print("monkey should jump");
+            Collider[] vineColliders = GetComponentsInParent<Collider>();
+            foreach (Collider c in vineColliders) c.enabled = false;
+            transform.SetParent(null);
+            rb.isKinematic = false;
+            rb.AddForce(Vector2.up * jumpForce);
+            rb.AddForce(Vector2.right * jumpForce);
+
+            MonkeyStates.currentState = MonkeyIs.JUMPING;
+        }
+    }
+}
---
+++ monkeyTest/Assets/Scripts/MonkeySwing.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeySwing : MonoBehaviour
+{
+    [SerializeField]
+    float swingForce = 50;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) SwingLeft();
+        if (Input.GetKeyDown(KeyCode.RightArrow)) SwingRight();
+    }
+
+    void SwingLeft()
+    {
+        if (MonkeyStates.currentState == MonkeyIs.HOLDING_VINE)
+        {
+            //rb.MovePosition(transform.position += (Vector3.left * swingForce));
+            rb.AddForce(Vector2.left * swingForce);
+        }
+    }
+
+    void SwingRight()
+    {
+        if (MonkeyStates.currentState == MonkeyIs.HOLDING_VINE)
+        {
+            //rb.MovePosition(transform.position += (Vector3.right * swingForce));
+            rb.AddForce(Vector2.right * swingForce);
+        }
+    }
+
+
+}
